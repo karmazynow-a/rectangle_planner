ec82badc7a159ed62d757dd7650c1d1c89550290
karmazynow-a <aleks.poreba97@gmail.com>
2020-05-25 12:04:14 +0200

    Add base

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+#include <cstdlib>
+#include <iostream>
+#include <ga/ga.h>
+
+#include "Board.h"
+
+// funkcja celu
+float objective(GAGenome &);
+
+int main () {
+
+	int popsize  = 100;
+	int ngen     = 1000;
+	float pcross = 0.5, pmut = 0.1;
+
+	// kodowanie binarne
+	GABin2DecPhenotype map;
+	map.add(30, 0, 1);
+	map.add(30, 0, 1);
+
+	GABin2DecGenome genome(map, objective);
+
+	GASimpleGA ga(genome);
+
+	// inicjacja algorytmu
+	ga.populationSize(popsize);
+	ga.nGenerations(ngen);
+	ga.pMutation(pmut);
+	ga.pCrossover(pcross);
+
+	// nieujemność f dostosowania
+	GASigmaTruncationScaling scaling;
+	ga.scaling(scaling);
+
+	// ustawienie selekcji
+	GARouletteWheelSelector s;
+	ga.selector(s);
+
+	// ustawienie krzyżowania
+	ga.crossover(GABin2DecGenome::TwoPointCrossover);
+
+	// sukcesja elitarna
+	ga.elitist(gaTrue); 
+
+	// zapis statystyk
+	ga.scoreFilename("zbieznosc.dat"); // plik z statystykami
+	ga.scoreFrequency(10); // zapis statystyk
+	ga.flushFrequency(50); // zapis do pliku
+	ga.selectScores(
+				GAStatistics::Mean | 
+                GAStatistics::Maximum | 
+                GAStatistics::Minimum |
+                GAStatistics::Deviation |
+                GAStatistics::Diversity);
+	
+	// ewolucja
+	ga.evolve((unsigned)time(0));
+
+	genome = ga.statistics().bestIndividual();
+
+	std::cout << "Najlepsze rozwiazanie : "
+			<< "F( x=" << genome.phenotype(0) 
+			<< ", y=" << genome.phenotype(1) 
+			<< ") = " << objective(genome) << std::endl;
+		
+	return 0;
+}
+
+
+float objective(GAGenome & c) {
+  GABin2DecGenome & genome = (GABin2DecGenome &)c;
+
+  double x = genome.phenotype(0);
+  double y = genome.phenotype(1);
+  return 0;
+}
+
